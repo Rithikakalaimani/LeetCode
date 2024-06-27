0ce8d87eccbfa5f92765d209e804c18c386a2c07
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2024-06-27 22:28:32 +0530

    problems : bs

---
+++ BinarySearch/Single_Element_Sorted_Array.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+import java.util.*;
+public class Single_Element_Sorted_Array {
+    public static int singleNonDuplicate(ArrayList<Integer> arr) {
+        int n = arr.size(); //size of the array.
+        int ans = 0;
+        // XOR all the elements:
+        for (int i = 0; i < n; i++) {
+            ans = ans ^ arr.get(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> arr =
+            new ArrayList<>(Arrays.asList(1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6));
+        int ans = singleNonDuplicate(arr);
+        System.out.println("The single element is: " + ans);
+    }
+}
+
